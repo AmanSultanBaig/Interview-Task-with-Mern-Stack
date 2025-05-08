3f7390df169db3598b7707937afae57d3e5a9c9c
Aman Sultan Baig <amansultanbaig@gmail.com>
2025-05-09 00:22:43 +0500

    distributed sliding-window rate limiter implemented

--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const reconciliationRoutes = require("./routes/reconciliationRoutes");
+const rateLimiterRoutes = require('./routes/rateLimiterRoutes');
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use('/api/', rateLimiterRoutes);
 app.use("/api/reconciliation", reconciliationRoutes);
+
 app.use(errorHandler);
 
 connectDB()
